43ff690dbc7a2cca8d3e4202d3838ff2be0d2ef7
wshngyf <8769634@qq.com>
2018-08-03 00:26:20 +0800

    json解析完成

--- scanmap/items.py
+++ scanmap/items.py
@@ -11,4 +11,15 @@ import scrapy
 class ScanmapItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    poi_name  = scrapy.Field
+    poi_typecode = scrapy.Field
+    poi_address = scrapy.Field
+    poi_pcode = scrapy.Field
+    poi_pname = scrapy.Field
+    poi_citycode = scrapy.Field
+    poi_adcode = scrapy.Field
+    poi_adname = scrapy.Field
+    poi_entr_location = scrapy.Field
+    poi_tel = scrapy.Field
+    poi_location = scrapy.Field
     pass
--- scanmap/spiders/scanmap_spider.py
+++ scanmap/spiders/scanmap_spider.py
@@ -1,10 +1,23 @@
 import scrapy
-
+import json
+from scanmap.items import ScanmapItem
 class ScanMapSpider(scrapy.Spider):
+	name = 'ScanMapSpider'
 	def start_requests(self):
-		urls = ['http://quotes.toscrape.com/page/1/',]
+		urls = ['http://restapi.amap.com/v3/place/text?key=6dfbd82a982fb0dab8fde453623e10ac&keywords=%E9%A4%90%E9%A5%AE&types=&city=%E6%B2%81%E9%98%B3&children=1&offset=20&page=1&extensions=all',]
 		for url in urls:
 			yield scrapy.Request(url=url,callback=self.parse)
 		
 	def parse(self,response):
+		content = response.body
+		items = []
+		if not content:
+			return
+		strjson = json.loads(content)
+		jsonarr = strjson['pois']
+		for i in jsonarr:
+			item = ScanmapItem()
+			item['poi_name']=i['name']
+			items.append(item)
+		return items
 	
