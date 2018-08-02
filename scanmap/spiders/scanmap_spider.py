a885eed8287c0a99c7efc343e36ce033a5112e5b
wshngyf <8769634@qq.com>
2018-08-02 17:25:00 +0800

    创建入口

--- scanmap/spiders/scanmap_spider.py
+++ scanmap/spiders/scanmap_spider.py
@@ -1,3 +1,10 @@
 import scrapy
 
 class ScanMapSpider(scrapy.Spider):
+	def start_requests(self):
+		urls = ['http://quotes.toscrape.com/page/1/',]
+		for url in urls:
+			yield scrapy.Request(url=url,callback=self.parse)
+		
+	def parse(self,response):
+	
